340d2207922cc9bd9133d259f8b30f4a4369a379
Cody Fisher <fishercody23@gmail.com>
2025-02-25 23:04:47 -0700

    Able to get weather data from API and pass a value back into the HTML

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 //Function to hit api (fetch)
+function fetchData() {
+    const apiKey = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/cochrane%20alberta?unitGroup=us&key=73ESG77VMVDHZPHKQ9YL7FNH4&contentType=json';
+    return fetch(apiKey, { mode: 'cors'})
+        .then((response) => {
+           if(!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`)
+           }
+           return response.json();
+        })
+        .catch((error) => {
+            console.log('Fetch error: ', error);
+        })
+} 
 
+function displayDesc() {
+    fetchData()
+    .then(weatherData => {
+        console.log(weatherData.days[0]);
+        if(weatherData && weatherData.days && weatherData.days[0]) {
+            document.getElementById('description').innerText = weatherData.days[0].description;
+        } else {
+            console.error('weatherData does not contain description')
+        }
+    }).catch(error => {
+        console.error('Error fetching data: ', error);
+    })
+}    
+
+displayDesc();
 //Function to process the api 
 
 //Form for user to get weather based on location
