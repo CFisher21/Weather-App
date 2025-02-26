58e936c3e09fb4189e75ae14180b4a4b3de8692f
Cody Fisher <fishercody23@gmail.com>
2025-02-25 22:11:41 -0700

    Base files set up and linked together.

---
+++ script.js
@@ -0,0 +1,7 @@
+//Function to hit api (fetch)
+
+//Function to process the api 
+
+//Form for user to get weather based on location
+
+// Add a loading screen from when the form is hit to when the api is displayed on the page
